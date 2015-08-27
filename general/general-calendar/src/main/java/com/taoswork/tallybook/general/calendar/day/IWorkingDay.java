293f514dc6f5366f1c9cfdee5f87da9a7c9cb732
Gao Yuan <andy.gao.y@gmail.com>
2015-08-27 15:52:42 +0800

    Calendar

---
+++ general/general-calendar/src/main/java/com/taoswork/tallybook/general/calendar/day/IWorkingDay.java
@@ -0,0 +1,13 @@
+package com.taoswork.tallybook.general.calendar.day;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by Gao Yuan on 2015/8/27.
+ */
+public class IWorkingDay {
+    Object[] /*day type*/ workingdays(){
+        return new Object[]{}; /*max size is 7*/
+    }
+}
